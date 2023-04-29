520d5d89f81e353ee71a4f829e59e8b04bd41039
Jeffrey A <agyepong99@gmail.com>
2023-04-29 06:08:23 -0600

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@
 module.exports = {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}', './node_modules/flowbite/**/*.js'],
   theme: {
+    colors: {
+      'bg-header-green': '#015104',
+    },
     extend: {},
   },
   plugins: [require('flowbite/plugin')],
